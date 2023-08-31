9d604f3d55aac06503cf76987f438a366366de9e
PavlenkoIGOR <120670456+PavlenkoIGOR@users.noreply.github.com>
2023-08-31 23:18:03 +0300

    Update HomeController.cs

--- AwesomeNet.Unit35/Controllers/HomeController.cs
+++ AwesomeNet.Unit35/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AwesomeNet.Unit35.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
